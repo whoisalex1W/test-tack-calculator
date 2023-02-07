0375cc52bd214ae8bf3f15bbe19d77a9ce98880b
Olexander <olexander@MacBook-Air-Olexander.local>
2023-02-07 17:56:34 +0200

    fix input and background

--- src/js/file.js
+++ src/js/file.js
@@ -11,44 +11,50 @@ function changeData() {
   var prices = document.getElementsByClassName("under-range__price");
   //counting price due to plan
   for (let index = 0; index < prices.length; index++) {
+    let storageValue = 0;
+    let transferValue = 0;
     switch (index) {
       case 0:
+        storageValue = 0.005;
+        transferValue = 0.01;
         let sum1 = (
-          storageRange.value * 0.005 +
-          transferRange.value * 0.01
+          storageRange.value * storageValue +
+          transferRange.value * transferValue
         ).toFixed(2);
-        // prices[index].innerHTML = sum1 >= 7 ? `${sum1}$` : "7$";
-        prices[index].innerHTML = `${sum1 >= 7 ? sum1 : 7}$` ;
+        prices[index].innerHTML = `${sum1 >= 7 ? sum1 : 7}`;
         break;
       case 1:
-        let subscriptionBunnyPrice = document.getElementById("bunny-radio-hdd")
-          .checked
+        storageValue = document.getElementById("bunny-radio-hdd").checked
           ? document.getElementById("bunny-radio-hdd").value
           : document.getElementById("bunny-radio-ssd").value;
+        transferValue = 0.01;
         let sum2 = (
-          storageRange.value * subscriptionBunnyPrice +
-          transferRange.value * 0.01
+          storageRange.value * storageValue +
+          transferRange.value * transferValue
         ).toFixed(2);
-        prices[index].innerHTML = sum2 <= 10 ? `${sum2}$` : "10$";
+        prices[index].innerHTML = sum2 <= 10 ? `${sum2}` : "10.00";
         break;
       case 2:
-        let subscriptionScalewayPrice = document.getElementById(
-          "scaleway-radio-multi"
-        ).checked
+        if(storageRange.value > 75) {
+          storageValue = document.getElementById("scaleway-radio-multi").checked
           ? document.getElementById("scaleway-radio-multi").value
           : document.getElementById("scaleway-radio-single").value;
+        }
+        transferValue = transferRange.value <= 75 ? 0 : 0.02;
+        
         let sum3 =
-          storageRange.value * subscriptionScalewayPrice +
-          transferRange.value * 0.02;
-        sum3 = (sum3 - 100 * subscriptionScalewayPrice).toFixed(2);
-        prices[index].innerHTML = sum3 <= 0 ? "0$" : `${sum3}$`;
+          (storageRange.value * storageValue + transferRange.value * transferValue);
+        sum3 = (sum3 - 100 * storageValue).toFixed(2);
+        prices[index].innerHTML = sum3 <= 0 ? "0" : `${sum3}`;
         break;
       case 3:
+        storageValue = 0.01;
+        transferValue = 0.01;
         let sum4 = (
-          storageRange.value * 0.01 +
-          transferRange.value * 0.01
+          storageRange.value * storageValue +
+          transferRange.value * transferValue
         ).toFixed(2);
-        prices[index].innerHTML = sum4 >= 5 ? `${sum4}$` : "5$";
+        prices[index].innerHTML = sum4 >= 5 ? `${sum4}` : "5";
         break;
       default:
         break;
@@ -71,7 +77,7 @@ function changeData() {
 
   for (let index = 0; index < ranges.length; index++) {
     ranges[index].style.width = `${
-      prices[index].innerHTML.replace("$", "") * 5
+      prices[index].innerHTML * 5
     }px`;
   }
   document.getElementById("input-volume").value = `${volume.value}`;
@@ -86,16 +92,16 @@ function changeRange(str) {
 
 function addColor(element) {
   switch (element.id.slice(-1)) {
-    case '1':
+    case "1":
       element.style.backgroundColor = "red";
       break;
-    case '2':
+    case "2":
       element.style.backgroundColor = "orange";
       break;
-    case '3':
+    case "3":
       element.style.backgroundColor = "purple";
       break;
-    case '4':
+    case "4":
       element.style.backgroundColor = "blue";
       break;
     default:
@@ -108,16 +114,14 @@ function removeColor(element) {
 }
 
 function minWidth(array) {
-  let minIndex = 0;
-  let minWidth = array[minIndex].style.width;
-  let minElement = array[minIndex];
+  let minWidth = array[0].style.width;
+  let minElement = array[0];
   for (let index = 0; index < array.length; index++) {
     if (
       parseFloat(array[index].style.width.replace("px", "")) <
       parseFloat(minWidth.replace("px", ""))
     ) {
       minWidth = array[index].style.width;
-      minIndex = index;
       minElement = array[index];
     }
   }
